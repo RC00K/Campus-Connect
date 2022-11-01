ac2d3da55b1bdb43e01b14faa8de595ff0619826
Ryder Cook <48497255+RC00K@users.noreply.github.com>
2022-10-31 19:14:49 -0500

    Updated Manifest & Service Worker

--- CampusConnect/sw.js
+++ CampusConnect/sw.js
@@ -1,3 +1,6 @@
+const staticCache = "Static-cache-v1";
+const dynamicCache = "Dynamic-cache-v1";
+
 const assets = [
     '/',
     "/index.html",
@@ -7,6 +10,17 @@ const assets = [
     "https://fonts.googleapis.com/icon?family=Material+Icons",
     "https://unicons.iconscout.com/release/v4.0.0/css/line.css"   
 ]
+
+// Cache sieze limit
+const limitCacheSize = (name, size) => {
+    caches.open(name).then((cache) => {
+        caches.open(name).then((keys) => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        })
+    })
+}
 
 self.addEventListener("install", function(event) {
     // Fires whent the browser installs the app
@@ -26,15 +40,36 @@ self.addEventListener("activate", function(event) {
     // Fires when the service worker completes its installation
     // A place for the service worker to clean up from
     // previous service worker versions
-    console.log(`SW: Event fired: ${event.type}`);
+    // console.log(`SW: Event fired: ${event.type}`);
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys
+                .filter((key) => key !== staticCache)
+                .map((key) => caches.delete(key))
+            );
+        })
+    );
 });
 
 self.addEventListener("fetch", function(event) {
     // Fires whenever the app requests a resource (file or data)
-    console.log(`SW: Event fired: ${event.request.url}`);
+    // console.log(`SW: Event fired: ${event.request.url}`);
     // Go get the requested resource from the network
-    event.respondWith(fetch(event.request));
-    caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-    });
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then((response) => {
+                return (
+                    response ||
+                    fetch(event.request).then((fetchRes) => {
+                        return caches.open('dynamicCache').then((cache) => {
+                            cache.put(event.request.url, fetchRes.clone());
+                            return fetchRes;
+                        });
+                    })
+                );
+            })
+            .catch(() => caches.match("/pages/fallback.html"))
+    );
 });
